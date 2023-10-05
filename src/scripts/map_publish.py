497555bc97226d0184ac14f43b1b1db8a7c45253
shin.hongjoong <theprismdata@gmail.com>
2023-10-05 02:16:53 -0700

    publish map edit

--- src/scripts/map_publish.py
+++ src/scripts/map_publish.py
@@ -3,22 +3,7 @@ import rospy
 from nav_msgs.msg import Odometry,OccupancyGrid
 from sensor_msgs.msg import LaserScan
 from tf2_msgs.msg import TFMessage
-import os
-import signal
-import json
 from std_msgs.msg import String
-
-# map_pub = rospy.Publisher('map_edit', OccupancyGrid, queue_size=1)
-map_pub = rospy.Publisher('map_edit', String, queue_size=1)
-
-def map_publish(map_opt):
-    map_msg = OccupancyGrid()
-    map_msg.data = map_opt.data
-    print(map_msg)
-    try:
-        map_pub.publish("map_edit")
-    except Exception as e:
-        print(str(e))
 
 def callback_map_scan(map_data):
     pose = map_data.info.origin
@@ -35,10 +20,19 @@ def callback_map_scan(map_data):
     map_dict = {'info':{'position':{'pos_x':pos_x,'pos_y':pos_y,'pos_z':pos_z},
                         'orientation': {'ori_x':ori_x,'ori_y':ori_y, 'ori_z':ori_z}}}
     print(map_dict)
-    map_publish(map_data)
+    map_pub = rospy.Publisher('map_edit', OccupancyGrid, queue_size=1)
+    # map_pub = rospy.Publisher('map_edit', String, queue_size=1)
+
+    map_msg = OccupancyGrid()
+    map_msg.data = data
+    try:
+        map_pub.publish(map_msg)
+    except Exception as e:
+        print(str(e))
 
 def listener():
     rospy.init_node('jetson_msg', anonymous=True)
+
     rospy.Subscriber("map", OccupancyGrid, callback_map_scan)
     
     rospy.spin()
